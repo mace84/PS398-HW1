e03150963425d1c5b118c290ff91c6150c0e5b18
Matthias Orlowski <mace84@gmail.com>
2012-02-06 12:15:40 -0500

    quick search and quick search with random pivot added

--- HW3.py
+++ HW3.py
@@ -1,41 +1,68 @@
 ## P398 Homework Assignment 3
 ## Matthias Orlowski
-## Implementation and test of two sorting algorithms () 
+## Implementation and test of two sorting algorithms (Insertion Sort and) 
 ## Algorithms wer implemented according to: http://www.sorting-algorithms.com/
 
+#! /usr/bin/python
+
 import random
+import csv
+import timeit
 
+# Exceptions:
+class MyException(Exception):
+    def __init__(self,value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+       
 # O(n^2) algorithm
-def insertionSort(list):
-    n = len(list)
-    for i in range(1,n):
-        k = i
-        for k in range(k,0,-1):
-            if k > 0 and list[k] < list[k-1]:
-                list[k], list[k-1] = list[k-1],list[k]
-    return list
+def insertionSort(inlist):
+    try:
+        if len(inlist) < 2:
+            return list
+        n = len(inlist)
+        for i in range(1,n):
+            k = i
+            for k in range(k,0,-1):
+                if k > 0 and inlist[k] < inlist[k-1]:
+                    inlist[k], inlist[k-1] = inlist[k-1],inlist[k]
+        return inlist
+    except:
+        if type(inlist) != list:
+            raise MyException, "This function only works in list objects. Please call on a list in the form [1,2,3]."
+
+# timeit.Timer("insertionSort()")
 
 
-#O(n^2) to O(n*log2(n)) algorithm
-def q3Sort(list):
-    n = len(list)
-    pivot = random.randrange(1,n)
-    list[n],list[pivot] = list[pivot], list[n]
+#O(n^2) to O(n) with average O(n*log2(n)) algorithm
+def quickSort(inlist):
+    try:
+        if inlist == []:
+            return []
+        else:
+            pivot = inlist[0]
+            lower = quickSort([i for i in inlist[1:] if i < pivot])
+            upper = quickSort([i for i in inlist[1:] if i >= pivot])
+            return lower + [pivot] + upper
+    except:
+        if type(inlist) != list:
+            raise MyException, "This function only works in list objects. Please call on a list in the form [1,2,3]."
 
-    i = 1; k = i; p = n
-    while i < p:
-        for i in range(0,1):
-            if list[i] < list[n]:
-                list[i], list[k] = list[k], list[i]
-                k += 1
-                break
-            elif list[i] == a[n]:
-                p -= 1
-                list[i], list[p] = list[p], list[i]
-                break
 
-    m = min(p-k,n-p+1)
-    
-                
-                
-        
+# add random pivot
+def quickSortRP(inlist):
+    try:
+        def qSort(inlist):
+            if inlist == []:
+                return []
+            else:
+                pivot = inlist.pop(random.randrange(len(inlist)))
+                lower = quickSort([i for i in inlist if i < pivot])
+                upper = quickSort([i for i in inlist if i >= pivot])
+                return lower + [pivot] + upper
+        return qSort(inlist)
+    except:
+        if type(inlist) != list:
+            raise MyException, "This function only works in list objects. Please call on a list in the form [1,2,3]."
