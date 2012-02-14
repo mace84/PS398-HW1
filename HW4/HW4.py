10da895e7915973263ae96546d60ef56284fc6a4
Matthias Orlowski <mace84@gmail.com>
2012-02-14 18:30:43 -0500

    Complexities added. Input checks still missing.

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -72,13 +72,15 @@ class LinkedList(object):
         
     def length(self):
         return self.head.height(self.head)
+            #best case: O(n), worst: O(n)
     
     def appendNode(self,new_value):
         if self.head == None:
             self.head = Node(new_value)
         else:
             self.head.appendChild(new_value)
-
+            #best case: O(n), worst: O(n)
+            
     def addNodeAfter(self, new_value, after_node):
         if after_node == 0:
             insertPoint = self.head
@@ -86,7 +88,8 @@ class LinkedList(object):
             insertPoint = self.head.index(after_node)
         addedNode = Node(new_value,insertPoint.next)
         insertPoint.next = addedNode
-
+            #best case: O(1), worst: O(n), depends on after_node chosen
+        
     def addNodeBefore(self, new_value, before_node):
         if before_node == 0:
             oldhead = self.head
@@ -95,7 +98,8 @@ class LinkedList(object):
             insertPoint = self.head.index(before_node - 1)
             addedNode = Node(new_value,insertPoint.next)
             insertPoint.next = addedNode
-
+            #best case: O(1), worst: O(n), depends on before_node chosen
+            
     def removeNode(self, node_to_remove):
         if node_to_remove == 0:
             newhead = self.head.next
@@ -107,7 +111,8 @@ class LinkedList(object):
         else:
             postRemNode = self.head.index(node_to_remove + 1)
             preRemNode.next = postRemNode
-
+            #best case: O(1), worst: O(2n), depends on node_to_remove chosen, if thats not 0, then time is at least n+1 due to checking the if condition
+            
     def removeNodesByValue(self, value):
         if self.length() == 1:
             if self.head.checkValues(value) == True:
@@ -119,24 +124,31 @@ class LinkedList(object):
             if checkelement.checkValues(value) == True:
                 self.removeNode(i)
                 self.removeNodesByValue(value)
-    # This raises an index out of bound error but generates the right result. Why?
+                # This raises an index out of bound error but generates the right result. Why?
+            #best case: O(n), worst: O(3n^2)
                 
     def reverse(self):
-        for i in range(1,self.length()):
-            newhead = self.head.index(i).value
-            self.addNodeBefore(newhead,0)
-            self.removeNode(i+1)
-        return self
-
+        try:
+            for i in range(1,self.length()):
+                newhead = self.head.index(i).value
+                self.addNodeBefore(newhead,0)
+                self.removeNode(i+1)
+                return self
+        except:
+            if self.hasCycle() == T:
+                raise Exceptions, "Reverse does not yet work for circular lists. Sorry, but we are working on it!"
+            # best: O(n^2), worst: O(n^2)
+            
     def addCycle(self):
         self.head.index(self.length()-1).next = self.head
-
+            # best = worst: O(n)
+        
     def hasCycle(self):
         for i in range(0,self.length()):
             if self.head.index(i).next == None:
                 return False
             elif i == self.length()-1:
                 return True
-
+            # best = worst: O(n^2)
+            
         # add checks for bad inputs (check index values for being integers)
-        # add complexities
