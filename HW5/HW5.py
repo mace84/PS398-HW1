0f75976865ebc22188013cba2a3ed5b23997fc8c
Matthias Orlowski <mace84@gmail.com>
2012-02-22 10:03:29 -0500

    Simple webcrawler works in that it collects what it should. Formatting of results is awful though. Need to extract information from strings.

---
+++ HW5/HW5.py
@@ -0,0 +1,93 @@
+## PS398 Homework 5
+## Matthias Orlowski
+## 02/21/12
+## Script implementing a basic webcrawler to grab liks and meta infos from Matt's blog at http://yspr.wordpress.com/
+
+#!/opt/local/bin/python2.7
+
+import urllib2
+import BeautifulSoup
+import csv
+import sys
+from time import sleep
+from datetime import date
+
+# First specify some little helpers. They are very specific to the formatting of the blog
+
+def stringOut(input):
+    mkstring = str(input)
+    tmp = mkstring.split(">")[1]
+    out = tmp.split("<")[0]
+    return out
+
+def authorOut(input):
+    mkstring = str(input)
+    tmp1 = mkstring.rsplit('rel="author"')[1]
+    tmp2 = tmp1.rsplit("<")[0]
+    out = tmp2.lstrip(">")
+    return out
+    
+def transDate(stringdate):
+    months = {"January" : 1,"February" : 2,"March" : 3,"April" : 4,"Mai" : 5,"June" : 6,"July" : 7,"August": 8,"September":9,"Oktober":10,"November":11,"December":12}
+    sdate = stringdate.split()
+    month = months[sdate[0]]
+    day = int(sdate[1].rstrip(","))
+    year = int(sdate[2])
+    outdate = date(year,month,day)
+    return outdate
+
+# now let's crawl
+
+root = 'http://yspr.wordpress.com/'
+
+post_title = []
+author = []
+publish_date = []
+urls = []
+comment_count = []
+tempurls = []
+
+# grab first site links
+home = urllib2.urlopen(root)
+shome = BeautifulSoup.BeautifulSoup(home.read())
+for link in shome('a'):
+    if root in link['href'] and link['href'] not in tempurls:
+        tempurls.append(link.get('href'))
+
+# now crawl them
+while len(tempurls) > 0:
+    for links in tempurls:
+        post = urllib2.urlopen(links)
+        spost = BeautifulSoup.BeautifulSoup(post.read())
+
+        for link in spost('a'):
+            if root in link['href'] and link['href'] not in urls and link['href'] not in tempurls:
+                tempurls.append(link.get('href'))
+
+        temptitle = spost.findAll("h1", {"class" : "entry-title"})
+        post_title.append(temptitle)
+
+        tempauthor = spost.findAll("a", {"rel":"author"})
+        author.append(tempauthor)
+
+        indate = spost.findAll("span", {"class" : "entry-date"})
+        publish_date.append(indate)
+
+        comments = spost.findAll("div", {"class":"comment-meta commentmetadata"})
+        comment_count.append(len(comments))
+
+        urls.append(tempurls.pop(0))
+
+        sleep(2)
+
+
+# generate output
+Output = []
+for i in range(0,len(post_title)):
+    Output.append([post_title[i],author[i],publish_date[i], urls[i], comment_count[i]])
+
+# write output into file
+with open('yspr_crawl.csv', 'wb') as file:
+  csvWriter = csv.writer(file)
+  for i in Output:
+    csvWriter.writerow(i)
